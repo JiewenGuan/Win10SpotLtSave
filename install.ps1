6fa2c0b5f86048cc1220c56feaacd04dd71c8b63
Jiewen Guan <heiye9608@gmail.com>
2022-09-28 06:39:55 +0800

    shortcut update

--- install.ps1
+++ install.ps1
@@ -2,6 +2,6 @@ python -m venv env
 ./env/Scripts/activate
 pip install -r requirements.txt
 echo "cd $pwd" | Out-File -FilePath ./resave.ps1
-echo "$pwd\env\Scripts\Activate.ps1" | Out-File -FilePath ./resave.ps1 -Append
-echo "python $pwd\resave.py" | Out-File -FilePath ./resave.ps1 -Append
+echo ".\env\Scripts\Activate.ps1" | Out-File -FilePath ./resave.ps1 -Append
+echo "python .\resave.py" | Out-File -FilePath ./resave.ps1 -Append
 echo "Use the resave.ps1 as a shortcut"
