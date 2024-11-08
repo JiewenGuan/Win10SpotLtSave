3ee390107dfe8233706177503134b0cfa82e9fd9
Jiewen Guan <heiye9608@gmail.com>
2024-11-08 20:37:42 +0800

    Update install.ps1
    bugfix

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 python -m venv env 
 ./env/Scripts/activate
-pip install -r requirements.txt
+pip install -r requirement.txt
 echo "cd $pwd" | Out-File -FilePath ./resave.ps1
 echo ".\env\Scripts\Activate.ps1" | Out-File -FilePath ./resave.ps1 -Append
 echo "python .\resave.py" | Out-File -FilePath ./resave.ps1 -Append
